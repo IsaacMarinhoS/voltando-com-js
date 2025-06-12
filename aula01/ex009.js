d3fbc6c82d636d28fed46cd7ce18b1b5790ea9db
IsaacMarinhoS <isaacrosemback0@gmail.com>
2025-06-11 21:40:59 -0300

    mais exrcicios completos

---
+++ aula01/ex009.js
@@ -0,0 +1,13 @@
+var vel = 40.5
+
+ console.log(`velocidade: ${vel}`)
+
+if (vel > 50) {
+   
+    console.log("Você foi multado!")
+    console.log("Use cinto de segurança!")
+
+} else {
+    
+    console.log("Use cinto de segurança!")
+}
